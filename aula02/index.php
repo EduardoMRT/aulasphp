0b534b276346d7244603786e0c42bff719c16808
Eduardo Miguel dos Reis Teixeira <eduksoficial@gmail.com>
2024-02-03 12:49:53 -0300

    aula02

---
+++ aula02/index.php
@@ -0,0 +1,12 @@
+<?php
+    $nome = 'Miguel';
+
+    if($nome != 'Eduardo'){
+        echo 'Bem vindo ' .$nome; 
+    }if($nome == 'Miguel'){
+        echo 'Usuario ADM ' .$nome;
+    }  else{
+       echo 'Acesso negado!'; 
+    }
+
+?>
